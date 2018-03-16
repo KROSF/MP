9b76fbfca0ab365637cd005b7bd85f7285d61b7c
Álvaro <alvaroperinan.business@gmail.com>
2018-03-16 11:43:59 +0100

    bla

--- vehiculos.c
+++ vehiculos.c
@@ -3,3 +3,28 @@
 //
 
 #include "vehiculos.h"
+#include "types.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void vehiculos(){
+    int op;
+
+    printf(" ______________________\n");
+    printf("|                      |\n");
+    printf("| 1. MIS VEHÍCULOS     |\n");
+    printf("| 2. AGREGAR VEHÍCULO  |\n");
+    printf("| 3. EDITAR VEHÍCULO   |\n");
+    printf("| 4. ELIMINAR VEHÍCULO |\n");
+    printf("|______________________|\n");
+
+    printf("Seleccione una opción: \n");
+    scanf("%i",&op);                                  //Este scanf recogerá la opción seleccionada por el usuario
+
+        switch(op){
+            case 1: ListaVehiculos
+
+    }
+
+
+}
