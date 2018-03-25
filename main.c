d6a7caf755ea7c9528a726555e987a0ecec6e9fe
Rodrigo Sanabria <rodrigosanabria22@gmail.com>
2018-03-25 12:33:39 +0200

    change funtion order

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 #include "guardar.h"
 int main(){
 
-    Usuarios *usuarios;
-    Vehiculos *vehiculos;
-    Viajes *viajes;
-    Pasos *pasos;
-    Incidencias *incidencias;
+    Usuarios* usuarios;
+    Vehiculos* vehiculos;
+    Viajes* viajes;
+    Pasos* pasos;
+    Incidencias* incidencias;
 
     int vector[5];
 
@@ -20,4 +20,3 @@ int main(){
 
     return 0;
 }
-
