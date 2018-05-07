2c2e38bfd6365a8a09b7bbac203081638a9c7fc8
Jesús <thejedex@gmail.com>
2018-05-07 11:50:19 +0200

    que era coña jajaja sube estos anda ;)

--- src/menu.h
+++ src/menu.h
@@ -1,8 +1,26 @@
 #ifndef MENU_H
 #define MENU_H
 #include "usuarios.h"
+
+/**
+ * muestra menú principal para usuarios
+ * @param v              referencia al vector user
+ * @param vi             referencia al vector inci
+ * @param vv             referencia al vector viajes
+ * @param vve            referencia al vector vehi
+ * @param indexusuario   Index del usuario
+ */
 void menuUser(vUsuarios *vu, vIncidencias *vi, vViajes *vv, vVehiculos *vve,
-              int id);
+              int indexusuario);
+
+/**
+ * muestra menú principal para administradores
+ * @param vu          referencia al vector user
+ * @param vi          referencia al vector inci
+ * @param vv          referencia al vector viajes
+ * @param vve         referencia al vector vehi
+ * @param indexadmin  Index del admin
+ */
 void menuAdmin(vUsuarios *vu, vIncidencias *vi, vViajes *vv, vVehiculos *vve,
-               int id);
+               int indexadmin);
 #endif
